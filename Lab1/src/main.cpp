62ac3581a2ca5a75d0c83cee8708172b8f44c22d
Edgar Zhavoronkov <edzhavoronkov@gmail.com>
2014-04-14 21:48:21 +0400

    Alpha ver. is done here

--- Lab1/src/main.cpp
+++ Lab1/src/main.cpp
@@ -4,10 +4,28 @@
 #include <string>
 
 
+bool all_theta_is_negative(std :: vector<double>& theat) {
+	for (int i = 0; i < theta.size(); ++i) {
+		if (theta[i] >= 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
 bool phi_is_null(std :: vector<std :: vector<double> >& table) {
 	for (int i = 0; i < table[0].size(); ++i) {
-		if (table[table.size() - 1][i] != 0){
+		if (table[table.size() - 1][i] != 0) {
 			return false;
+		}
+	}
+	return true;
+}
+
+bool target_row_contains_positive_elements(std :: vector<std :: vector<double> >& table) {
+	for (int i = 0; i < table[0].size(); ++i) {
+		if (table[table.size() - 2][i] < 0) {
+			return false;	
 		}
 	}
 	return true;
@@ -43,48 +61,45 @@ void recalc_table(std :: vector<std :: vector<double> >& table, double solving_e
 	}
 }
 
-double get_solving_element(std :: vector<std :: vector<double> >& table, int n, int m, int& s, int& r, std :: string mode) {
+double get_solving_element(std :: vector<std :: vector<double> >& table, int b_var, int s_var, int& s, int& r, std :: string mode) {
 	int offset;
 	int rows = table.size();
 	int columns = table[0].size();
-
 	if (mode == "phi") {
 		offset = 1;
 	} else if (mode == "zet") {
 		offset = 2;
 	}
-
 	std::vector<double> theta;
-	theta.assign(m, 0);
-
+	theta.assign(b_var, 0);
 	double min = 1e9;
 	r = 0;
-
-	for (int i = 1; i <= n; ++i){
+	for (int i = 1; i <= s_var + b_var; ++i) {
 		if (table[rows - offset][i] < min) {
 			min = table[rows - offset][i];
 			r = i;
 		}
 	}
-
-	for (int i = 0; i < m; ++i) {
+	for (int i = 0; i < b_var; ++i) {
 		theta[i] = table[i][0] / table[i][r];
 	}
-
-	min = 1e9;
-	s = 0;
-
-	for (int i = 0; i < m; ++i) {
-		if (theta[i] > 0 && theta[i] < min) {
-			min = theta[i];
-			s = i;
+	if (all_theta_is_negative(theta)) {
+		std :: cout << "No solution!" << std :: endl;
+		exit(-1);
+	} else {
+		min = 1e9;
+		s = 0;
+		for (int i = 0; i < b_var; ++i) {
+			if (theta[i] > 0 && theta[i] < min) {
+				min = theta[i];
+				s = i;
+			}
 		}
+		return table[s][r];
 	}
-
-	return table[s][r];
 }
 
-std :: vector<int>& gen_plan(std :: vector<std :: vector<double> >& table, int rows, int columns, int n, int m){
+std :: vector<double>& gen_plan(std :: vector<std :: vector<double> >& table, int rows, int columns, int b_var, int s_var) {
 	if (!all_constants_are_not_negative(table)) {
 		for (int i = 0; i < rows - 2; ++i) {
 			if (table[i][0] < 0) {
@@ -96,17 +111,26 @@ std :: vector<int>& gen_plan(std :: vector<std :: vector<double> >& table, int r
 		//recalc by phi string
 		while (!phi_is_null(table)) {
 			int s,r;
-			double solving_element = get_solving_element(table, n, m, s,r,"phi");
+			double solving_element = get_solving_element(table, b_var, s_var, s, r, "phi");
+			std :: cout << solving_element << std :: endl;
 			recalc_table(table, solving_element, s, r);
 		}
 		//recalc by target function string
 		int s,r;
-		double solving_element = get_solving_element(table, n, m, s,r,"zet");
+		double solving_element = get_solving_element(table, b_var, s_var, s, r, "zet");
+		std :: cout << solving_element << std :: endl;
 		recalc_table(table, solving_element, s, r);
 	} else {
-		int s,r;
-		double solving_element = get_solving_element(table, n, m, s,r,"zet");
-		recalc_table(table, solving_element, s, r);
+		while (!target_row_contains_positive_elements(table)) {
+			int s,r;
+			double solving_element = get_solving_element(table, b_var, s_var, s, r, "zet");
+			std :: cout << solving_element << std :: endl;
+			recalc_table(table, solving_element, s, r);
+			if (phi_is_null(table)) {
+				std :: cout << "No solution!" << std :: endl;
+				exit(-1);
+			}
+		}
 	}	
 }
 
@@ -114,15 +138,11 @@ std :: vector<int>& gen_plan(std :: vector<std :: vector<double> >& table, int r
 int main(int argc, char** argv) {
 	const char* input_file = argv[1];
 	const char* output_file = argv[2];
-
 	std :: ifstream ifs(input_file, std :: ifstream :: in);
 	std :: ofstream ofs(output_file, std :: ifstream :: out); 
-	
 	std :: vector<std :: vector<double> > table;
-
 	int n, m, b_var, s_var;
 	ifs >> n >> m >> b_var >> s_var;
-
 	for (int i = 0; i < n; ++i) {
 		std :: vector <double> tmp;
 		for (int j = 0; j < m; ++j) {
@@ -132,13 +152,10 @@ int main(int argc, char** argv) {
 		}
 		table.push_back(tmp);
 	}
-
 	std :: vector<double> phi;
 	phi.assign(m, 0);
 	table.push_back(phi);
-
 	gen_plan(table, table.size(), table[0].size(), b_var, s_var);
-
 	for (int i = 0; i < table.size(); ++i) {
 		for (int j = 0; j < table[i].size(); ++j) {
 			ofs << table[i][j] << " ";
